6c95b6ee781a8ab5d04e975841e8c3bd58859a99
flipster23 <37265235+flipster23@users.noreply.github.com>
2021-02-17 14:01:01 +0000

    pulled all files out into one folder

--- TwitchMon/TwitchMon/WorldMap.gd
+++ TwitchMon/TwitchMon/WorldMap.gd
@@ -7,10 +7,10 @@ extends Node2D
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	var overlay = load("res://DebugOverlay/debug_overlay.tscn").instance()
-	overlay.add_stat("Player position", $Player, "position", false)
-	add_child(overlay)
+#func _ready():
+#	var overlay = load("res://DebugOverlay/debug_overlay.tscn").instance()
+#	overlay.add_stat("Player position", $Player, "position", false)
+#	add_child(overlay)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
