c6968c5be0ae0031277174e33bcba4e34b6428dd
PiotrSiemaszko <79026673+PiotrSiemaszko@users.noreply.github.com>
2021-02-25 15:51:13 +0100

    Added Options

---
+++ TwitchMon/TwitchMon/WorldMap/GUI.gd
@@ -0,0 +1,18 @@
+extends CanvasLayer
+
+
+func _input(event):
+	if event.is_action_pressed("GameMenu"):
+		get_node("GameMenu").show()
+
+func _on_OptionBtn_pressed():
+	var options_menu = load("res://GUI Scenes/OptionsMenu.tscn").instance()
+	add_child(options_menu)
+	get_node("OptionsMenu").connect("CloseOptions",self,"CloseOptions")
+
+
+func CloseOptions():
+	get_node("OptionsMenu").queue_free()
+
+func _on_ExitBtn_pressed():
+	get_node("GameMenu").hide()
