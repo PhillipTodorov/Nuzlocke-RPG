a3c299a42eedfee7ab30979e5c30751a8b774578
flipster23 <44142049+PhillipTodorov@users.noreply.github.com>
2021-04-24 22:06:45 +0100

    added levitating tween to spirits in battle

---
+++ TwitchMon/TwitchMon/Spirits/General/AnimationHandler.gd
@@ -0,0 +1,40 @@
+extends Node
+
+onready var move_tween = get_parent().get_node("Tween")
+onready var icon = get_parent().get_node("icon")
+
+#if even then 'move_up' should 
+#be executed, otherwise,
+#execute 'move_down'
+onready var tween_state = 0 
+onready var rng = RandomNumberGenerator.new()
+
+
+
+func _ready():
+	move_tween.connect("tween_all_completed", self, "levitate")
+	yield(get_tree().create_timer(rng.randf()), "timeout")
+	levitate()
+	pass
+
+
+
+func levitate():
+	if tween_state % 2 == 0: 
+		move_up()
+	else:
+		move_down()
+	tween_state += 1
+
+
+
+func move_up():
+	move_tween.interpolate_property(icon, "offset", icon.offset, Vector2(0,-10), 2, Tween.EASE_IN, Tween.EASE_OUT)
+	move_tween.start()
+
+
+
+func move_down():
+	move_tween.interpolate_property(icon, "offset", icon.offset, Vector2(0,15), 2, Tween.EASE_IN, Tween.EASE_OUT)
+	move_tween.start()
+
